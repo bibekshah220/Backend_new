3eb15f775434cd0499a929d3cfe13deeaeb18d3f
bibekshah220 <bibekshah425@gmail.com>
2026-04-19 21:17:29 +0545

    feat: add new endpoint for twitter API

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const port = 8080;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get("twitter",(req,res)=>{
+    res.send("twitter api");
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
